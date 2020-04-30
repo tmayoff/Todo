64c946a1bdbbe774efbdc94cc9709af856dd46d3
Tyler <tyler@tylermayoff.com>
2020-04-30 11:11:23 -0400

    Add port option for DB

--- services/sequelize.js
+++ services/sequelize.js
@@ -2,7 +2,7 @@ const { Sequelize}  = require('sequelize');
 const sequelize = new Sequelize({
 	dialect: 'mariadb',
 	host: process.env.DB_HOST,
-	port: 32777,
+	port: process.env.DB_PORT,
 	database: process.env.DB_NAME,
 	username: process.env.DB_USER,
 	password: process.env.DB_PASS,
